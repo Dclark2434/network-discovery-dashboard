a70013f982b367c7ff25ebe5d3cd82529828d6f9
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-05-30 15:14:44 -0500

    Implement is_alive function to check if IP is responding on port 80

--- scanner/discover.py
+++ scanner/discover.py
@@ -1,9 +1,17 @@
 import socket
-import ipaddress
-from concurrent.futures import ThreadPoolExcecutor
 
 def is_alive(ip):
-    pass
+    """A function that utilizes the socket module to check if port 80 is responding at
+    the passed in IP.
+
+    https://docs.python.org/3/library/socket.html#socket.socket.connect
+
+    :param ip: string - The IPv4 address that we are checking."""
+    try:
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((str(ip), 80))
+        return str(ip)
+    except:
+        return None
 
 def scan_subnet(subnet):
     pass
@@ -11,4 +19,4 @@ def scan_subnet(subnet):
 if __name__ == "__main__":
     subnet = "192.168.1.0/24"
     live = scan_subnet(subnet)
-    print(f"discovered {len(live)} hosts.")
+  #  print(f"discovered {len(live)} hosts.")
