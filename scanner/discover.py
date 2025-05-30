fbce34323cab3511ffe0d20e5f737b6ca5d6aaf8
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2025-05-30 15:17:17 -0500

    Refactor docstring in is_alive function for clarity and completeness

--- scanner/discover.py
+++ scanner/discover.py
@@ -1,12 +1,14 @@
 import socket
 
 def is_alive(ip):
-    """A function that utilizes the socket module to check if port 80 is responding at
-    the passed in IP.
+    """A function that utilizes the socket module to check if port 80 is responding 
+    at the passed in IP.
 
     https://docs.python.org/3/library/socket.html#socket.socket.connect
 
-    :param ip: string - The IPv4 address that we are checking."""
+    :param ip: string - The IPv4 address that we are checking.
+    :return: string or None - If port 80 is open on target ip it will return the ip
+    for later storage, otherwise it will return None"""
     try:
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((str(ip), 80))
         return str(ip)
